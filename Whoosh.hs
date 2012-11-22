9da6bf522ed4390af5f762382ed09c11c2124829
Benjamin Saunders <besaunde@sfu.ca>
2012-11-22 00:38:21 -0800

    Style

--- Whoosh.hs
+++ Whoosh.hs
@@ -43,8 +43,9 @@ uniform a b = state $ randomR (a, b)
 genMV :: Bullet -> State StdGen Double
 genMV (Bullet caliber _ ogiveLen _ _) = do
   let sharpness = ogiveLen / caliber
-  muzVee <- if sharpness > 1.5 then lognormal 0.2 0.5 (200+(log(sharpness*2)*200)) 200 else
-                                    lognormal 0.5 0.5 250 100
+  muzVee <- if sharpness > 1.5
+            then lognormal 0.2 0.5 (200+(log(sharpness*2)*200)) 200
+            else lognormal 0.5 0.5 250 100
   return (muzVee)
 
 genEnergy :: State StdGen Double
